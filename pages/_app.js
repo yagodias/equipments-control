ab7b11ca0bfc647594e68373661c8991cb287272
Yago Dias <yago.dias1409@gmail.com>
2023-03-23 00:58:29 -0300

    fix: Adding new dependencie to style and removing initial content

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
 
+const inter = Inter({ subsets: ['latin'] });
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+          <title>Controle dos equipamentos</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
